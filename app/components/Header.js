0f914b5e11cc1fed11e534a01b0bd5c58e4ef2d1
CoderBoy M J H Nahid <coderboymjhnahid@gmail.com>
2024-11-08 02:03:39 +0600

    add header

---
+++ app/components/Header.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header id="header">
+      <nav className="container m-auto">
+        <div className="flex items-center justify-center py-6">
+          <div className="logo_wrapper w-2/6">
+            <Image
+              src={`/logo-portada.png`}
+              width={150}
+              height={45}
+              alt="Logo Image"
+              className="m-auto"
+            />
+          </div>
+          <div className="menu_wrapper w-4/6">
+            <ul className="uppercase flex items-center justify-center gap-9">
+              <li>
+                <Link href={`/`}>Inicio</Link>
+              </li>
+              <li>
+                <Link href={`/`}>Empresa</Link>
+              </li>
+              <li>
+                <Link href={`/`}>Shop</Link>
+              </li>
+              <li>
+                <Link href={`/`}>Consejos</Link>
+              </li>
+              <li>
+                <Link href={`/`}>Contacto Redes</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
