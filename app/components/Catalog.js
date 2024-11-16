ec5b6845abd5bc708877d374be60855696044399
CoderBoy M J H Nahid <coderboymjhnahid@gmail.com>
2024-11-16 22:03:12 +0600

    fix a issue

--- app/components/Catalog.js
+++ app/components/Catalog.js
@@ -4,20 +4,17 @@ import React from "react";
 
 const Catalog = () => {
   return (
-    <section
-      id="catalog"
-      className="py-8"
-    >
+    <section id="catalog" className="py-8">
       <div className="container m-auto text-center text-white">
         <h2 className="text-4xl mb-4">Catálogo Pura+</h2>
         <p className="font-light text-sm">
           Mira nuestro amplio portafolio de productos.
         </p>
         <p className="font-light text-sm">
-          "Nuestro lema es inspira y transpira"
+          &quot;Nuestro lema es inspira y transpira&quot;
         </p>
         <p className="font-light text-sm">
-          "Inspiration and perspiration is our motto"
+          &quot;Inspiration and perspiration is our motto&quot;
         </p>
         <Link href={`https://catalogo.puramas.co/`} target="_blank">
           <Image
